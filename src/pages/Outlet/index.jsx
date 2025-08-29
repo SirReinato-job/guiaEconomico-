0aa8aaa6f7a28cb29cad738bc722398419cf242b
Renato <renatofranca2024@gmail.com>
2025-08-29 17:21:06 -0300

    definindo container

--- src/pages/Outlet/index.jsx
+++ src/pages/Outlet/index.jsx
@@ -1,9 +1,37 @@
-import { Outlet } from "react-router";
+// ContainerGeral.jsx
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
 
 export default function ContainerGeral() {
   return (
-    <div>
-      <Outlet />
-    </div>
+    <Container>
+      <NavContainer>
+        <h1>Nav</h1>
+        <h1>aaaaaaaaaaaaa</h1>
+      </NavContainer>
+      
+      <MainContainer>
+        <Outlet />
+      </MainContainer>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 15% 83%;
+  grid-template-areas: "nav main";
+  grid-column-gap: 18px;
+  height: 100vh;
+`;
+
+const NavContainer = styled.div`
+  grid-area: nav;
+  background-color: red;
+`;
+
+const MainContainer = styled.div`
+  grid-area: main;
+  background-color: blue;
+  overflow-y: auto;
+`;
